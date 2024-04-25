19b6315b8bba04f13f046f560f614e3b2c21333b
Äkwav <ekwav@coflnet.com>
2024-04-25 20:57:00 +0200

    batch updates

--- Services/MigrationService.cs
+++ Services/MigrationService.cs
@@ -88,7 +88,7 @@ namespace Coflnet.Payments.Services
             var i = 0;
             var toUpdate = context.OwnerShips.Where(o => o.UserId == null).Select(o => o.Id).OrderByDescending(id=>id);
             var ids = new HashSet<long>(toUpdate);
-            var batchSize = 1000;
+            var batchSize = 50;
             logger.LogInformation($"Migrating {ids.Count} OwnerShips currently null");
             for (int j = 0; j < ids.Count; j += batchSize)
             {
@@ -103,23 +103,37 @@ namespace Coflnet.Payments.Services
                 logger.LogInformation($"Migrated {j + batchSize} OwnerShips");
             }
 
-            foreach (var item in oldDb.PaymentRequests.Select(o => new { o.Id, UserId = o.User.Id }).OrderByDescending(o => o.Id))
+            var transactionsToupdate = context.FiniteTransactions.Where(o => o.ProductId == null).Select(o => o.Id).OrderByDescending(id => id);
+            ids = new HashSet<long>(transactionsToupdate);
+            logger.LogInformation($"Migrating {ids.Count} FiniteTransactions currently null");
+            for (int j = 0; j < ids.Count; j += batchSize)
             {
-                context.PaymentRequests.Where(o => o.Id == item.Id).ExecuteUpdate(o => o.SetProperty(p => p.UserId, item.UserId));
-                if (++i % 100 != 0)
-                    continue;
+                var batch = ids.Skip(j).Take(batchSize);
+                var toBeupdated = context.FiniteTransactions.Where(o => batch.Contains(o.Id));
+                var idLookup = await oldDb.FiniteTransactions.Where(o => batch.Contains(o.Id)).ToDictionaryAsync(o => o.Id);
+                foreach (var item in toBeupdated)
+                {
+                    item.ProductId = idLookup[item.Id].ProductId;
+                }
                 await context.SaveChangesAsync();
-                logger.LogInformation($"Migrated {i} PaymentRequests");
-            }
-            foreach (var item in oldDb.FiniteTransactions.Select(o => new { o.Id, ProductId = o.Product.Id }).OrderByDescending(o => o.Id))
-            {
-                context.FiniteTransactions.Where(o => o.Id == item.Id).ExecuteUpdate(o => o.SetProperty(p => p.ProductId, item.ProductId));
-                if (++i % 100 != 0)
-                    continue;
-                await context.SaveChangesAsync();
-                logger.LogInformation($"Migrated {i} FiniteTransactions");
+                logger.LogInformation($"Migrated {j + batchSize} FiniteTransactions");
             }
 
+            var paymentRequestsToupdate = context.PaymentRequests.Where(o => o.UserId == null).Select(o => o.Id).OrderByDescending(id => id);
+            var intIds = new HashSet<int>(paymentRequestsToupdate);
+            logger.LogInformation($"Migrating {intIds.Count} PaymentRequests currently null");
+            for (int j = 0; j < intIds.Count; j += batchSize)
+            {
+                var batch = intIds.Skip(j).Take(batchSize);
+                var toBeupdated = context.PaymentRequests.Where(o => batch.Contains(o.Id));
+                var idLookup = await oldDb.PaymentRequests.Where(o => batch.Contains(o.Id)).ToDictionaryAsync(o => o.Id);
+                foreach (var item in toBeupdated)
+                {
+                    item.UserId = idLookup[item.Id].UserId;
+                }
+                await context.SaveChangesAsync();
+                logger.LogInformation($"Migrated {j + batchSize} PaymentRequests");
+            }
             //await MoveInt(oldDb.Users, context);
             //await MoveLongId(oldDb.PlanedTransactions, context); done
             // await MoveInt(oldDb.Products, context);
