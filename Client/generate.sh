b8120df01c0f5f5b0912369cb0d5c1dccc07c797
Äkwav <ekwav@coflnet.com>
2024-04-25 12:35:17 +0200

    generate .net 6 client

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,14 +1,18 @@
-VERSION=0.12.0
+VERSION=0.12.1
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5020/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Payments.Client,packageVersion=$VERSION,licenseId=MIT
+-o /local/out --additional-properties=packageName=Coflnet.Payments.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-sed -i 's/GIT_USER_ID/Coflnet/g' src/Coflnet.Payments.Client/Coflnet.Payments.Client.csproj
-sed -i 's/GIT_REPO_ID/Payments/g' src/Coflnet.Payments.Client/Coflnet.Payments.Client.csproj
-sed -i 's/>OpenAPI/>Coflnet/g' src/Coflnet.Payments.Client/Coflnet.Payments.Client.csproj
+path=src/Coflnet.Payments.Client/Coflnet.Payments.Client.csproj
+sed -i 's/GIT_USER_ID/Coflnet/g' $path
+sed -i 's/GIT_REPO_ID/Payments/g' $path
+sed -i 's/>OpenAPI/>Coflnet/g' $path
+sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
+sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
+
 
 FlagFile="src/Coflnet.Payments.Client/Model/RuleFlags.cs"
 sed -i 's/= 1/= 0/g' $FlagFile
@@ -20,4 +24,4 @@ sed -i 's/= 6/= 16/g' $FlagFile
 sed -i 's/= 7/= 32/g' $FlagFile
 
 dotnet pack
-cp src/Coflnet.Payments.Client/bin/Debug/Coflnet.Payments.Client.*.nupkg ..
+cp src/Coflnet.Payments.Client/bin/Release/Coflnet.Payments.Client.*.nupkg ..
